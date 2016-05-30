460841ee22f92e37f48876a698a9e4dd314fcea8
Javier Díaz <jdiaz@teravisiontech.com>
2016-05-30 17:38:20 -0400

    Pantalla del login lista.

---
+++ app/src/main/java/com/appcontactos/javierdiaz/jeunessemiami/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.appcontactos.javierdiaz.jeunessemiami.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.appcontactos.javierdiaz.jeunessemiami.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
