daea9ed13c1e70524208ee6c733999155ae5ce22
Javier Diaz <ibmjavier89@gmail.com>
2016-05-30 23:17:35 -0430

    -Se esta modificando el xml de MainActivity para que tenga el listview. -Metodo para validar campos en el Login

--- app/src/main/java/com/appcontactos/javierdiaz/jeunessemiami/activities/LoginActivity.java
+++ app/src/main/java/com/appcontactos/javierdiaz/jeunessemiami/activities/LoginActivity.java
@@ -2,14 +2,38 @@ package com.appcontactos.javierdiaz.jeunessemiami.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.appcontactos.javierdiaz.jeunessemiami.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView txtUser;
+    private TextView txtPass;
+    private Button btnLogin;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtUser = (TextView) findViewById(R.id.login_username);
+        txtPass = (TextView) findViewById(R.id.login_password);
+        btnLogin =(Button) findViewById(R.id.login_button);
     }
+
+    /*
+    Metodo para validar si los campos de user y pass no estan vacios.
+     */
+    private boolean validarCampos(TextView user, TextView pass){
+        if((user.length() == 0) || (pass.length() == 0)){
+             return false;
+        }else{
+        return true;
+        }
+    }
+
+
 }
