14bc8b5adddf8d2b9b646c61dbf9e2b0fe7fa8c3
Javier Diaz <ibmjavier89@gmail.com>
2016-06-20 01:02:11 -0430

    Segunda fase(Envio de SMS): Ya se tiene el menu de navegación.

---
+++ app/src/main/java/com/appcontactos/javierdiaz/jeunessemiami/activities/NevigationActivity.java
@@ -0,0 +1,148 @@
+package com.appcontactos.javierdiaz.jeunessemiami.activities;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.appcontactos.javierdiaz.jeunessemiami.MainActivity;
+import com.appcontactos.javierdiaz.jeunessemiami.R;
+import com.appcontactos.javierdiaz.jeunessemiami.fragments.SendsSmsFragment;
+import com.appcontactos.javierdiaz.jeunessemiami.fragments.SincronizarFragment;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class NevigationActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nevigation);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        setFragment(0);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.nevigation, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.nav_sincronizar:
+                setFragment(0);
+                break;
+            case R.id.nav_enviarsms:
+                setFragment(1);
+                break;
+            case R.id.nav_logout:
+                setFragment(2);
+                break;
+            default:
+                setFragment(0);
+                break;
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+
+    public void setFragment(int position) {
+        FragmentManager fragmentManager;
+        FragmentTransaction fragmentTransaction;
+        switch (position) {
+            case 0:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                SincronizarFragment inboxFragment = new SincronizarFragment();
+                fragmentTransaction.replace(R.id.fragment, inboxFragment);
+                fragmentTransaction.commit();
+                break;
+            case 1:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                SendsSmsFragment sendsSmsFragment = new SendsSmsFragment();
+                fragmentTransaction.replace(R.id.fragment, sendsSmsFragment);
+                fragmentTransaction.commit();
+                break;
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+    }
+}
---
+++ app/src/main/java/com/appcontactos/javierdiaz/jeunessemiami/fragments/SincronizarFragment.java
@@ -0,0 +1,306 @@
+package com.appcontactos.javierdiaz.jeunessemiami.fragments;
+
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.appcontactos.javierdiaz.jeunessemiami.R;
+import com.appcontactos.javierdiaz.jeunessemiami.activities.LoginActivity;
+import com.appcontactos.javierdiaz.jeunessemiami.adaptadores.CustomArrayAdapter;
+import com.appcontactos.javierdiaz.jeunessemiami.modelos.RowContactsModel;
+import com.appcontactos.javierdiaz.jeunessemiami.util.Config;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class SincronizarFragment extends Fragment implements View.OnClickListener {
+
+
+    private String phoneNumber;
+    private ListView listView_contactos;
+    private ArrayList<RowContactsModel> rows = new ArrayList<>();
+    private HashMap<Double, RowContactsModel> listItems;
+    private Button btnSincronizar;
+    private CustomArrayAdapter customArrayAdapter;
+    private Button btn_selecc_all;
+    private Button btn_unselecc_all;
+    private int pendingRequests = 0;
+    /**
+     * activity progress dialog
+     */
+    protected ProgressDialog mProgressDialog;
+
+
+    public SincronizarFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sincronizar, container, false);
+        listView_contactos = (ListView) view.findViewById(R.id.listview_contactos);
+        btnSincronizar = (Button) view.findViewById(R.id.btn_sincronizar);
+        btn_selecc_all = (Button) view.findViewById(R.id.btn_selecctodos);
+        btn_unselecc_all = (Button) view.findViewById(R.id.btn_deselecctodos);
+        btnSincronizar.setOnClickListener(this);
+        btn_selecc_all.setOnClickListener(this);
+        btn_unselecc_all.setOnClickListener(this);
+        listItems = new HashMap<>();
+
+        mProgressDialog = new ProgressDialog(getContext());
+
+        getNumber(getActivity().getContentResolver());
+
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    public void getNumber(ContentResolver cr) {
+        Cursor phones = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
+        while (phones.moveToNext()) {
+            RowContactsModel row = new RowContactsModel();
+            String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String lastname = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHONETIC_NAME));
+            String user_id = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID));
+            phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            System.out.println(name + " .................. " + phoneNumber + "ID............." + user_id);
+            row.setName(name);
+            row.setMobile_number(phoneNumber);
+            if (lastname != null) {
+                row.setSurname(lastname);
+            }
+            if (user_id != null) {
+                row.setUserid(user_id);
+            }
+
+            row.setChecked(true);
+            Cursor emails = cr.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, null, ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = " + user_id, null, null);
+            while (emails.moveToNext()) {
+                String email1 = emails.getString(emails.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+                row.setEmail(email1);
+                break;
+            }
+            emails.close();
+
+            listItems.put(getFotmatedNumber(phoneNumber), row);
+
+        }
+        phones.close();// close cursor
+        rows.addAll(listItems.values());
+        Collections.sort(rows, new Comparator<RowContactsModel>() {
+            public int compare(RowContactsModel v1, RowContactsModel v2) {
+                return v1.getName().compareToIgnoreCase(v2.getName());
+            }
+        });
+
+        listView_contactos.setItemsCanFocus(true);
+        customArrayAdapter = new CustomArrayAdapter(getActivity(), rows);
+        listView_contactos.setAdapter(customArrayAdapter);
+
+
+    }
+
+    /*
+    Metodo que formatea el num de tlf para usarlo como un key en el hashmap y no se repitan los num
+     */
+    private static Double getFotmatedNumber(String number) {
+        Double result = 0.0;
+        try {
+            number = number.replace(" ", "");
+            number = number.replace("+", "");
+            number = number.replace("-", "");
+            result = Double.valueOf(number);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * show a progress dialog with a custom message
+     *
+     * @param message
+     */
+    public void showProgressDialog(String message) {
+        if (mProgressDialog != null) {
+            mProgressDialog.setMessage(message);
+            mProgressDialog.show();
+        } else {
+            Log.e("MainActivity.class", "Error al mostrar el Progress Dialog");
+        }
+    }
+
+    /**
+     * hide the progress dialog
+     */
+    public void dismissProgressDialog() {
+        if (mProgressDialog != null) {
+            if (mProgressDialog.isShowing()) {
+                mProgressDialog.dismiss();
+            }
+        } else {
+            Log.e("MainActivity.class", "Error al mostrar el Progress Dialog");
+        }
+    }
+
+    private void selectAll() {
+
+        for (int i = 0; i < rows.size(); i++) {
+            rows.get(i).setChecked(true);
+        }
+        customArrayAdapter.notifyDataSetChanged();
+
+
+    }
+
+    private void unSelectAll() {
+
+        for (int i = 0; i < rows.size(); i++) {
+            rows.get(i).setChecked(false);
+        }
+        customArrayAdapter.notifyDataSetChanged();
+    }
+    /*
+    Método para sincrinizar los contactos en bd remota. No se sincroniza por lote.
+     */
+
+    public void synContacto(Context context, final String userid, final String nombre, final String apellido, final String correo, final String tlf) {
+        String url = Config.url_sincronizar;
+        RequestQueue queue = Volley.newRequestQueue(context);
+        StringRequest sr = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("INSERTAR CONTACTO", response);
+                pendingRequests--;
+                if (pendingRequests == 0) {
+                    dismissProgressDialog();
+                    Toast.makeText(getActivity(), "Usuarios sincronizados satisfactoriamente", Toast.LENGTH_LONG).show();
+
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                pendingRequests--;
+                if (pendingRequests == 0) {
+                    dismissProgressDialog();
+                    Toast.makeText(getActivity(), "Usuarios sincronizados con errores", Toast.LENGTH_LONG).show();
+
+                }
+                Log.d("ERROR AL INSERTAR", error.toString());
+                //Toast.makeText(getApplicationContext(), error.getMessage(),Toast.LENGTH_LONG).show();
+
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("method", "addContactsLot");
+                params.put("param1[]", LoginActivity.userid);
+                params.put("param2[]", nombre);
+                if (apellido != null) {
+                    params.put("param3[]", apellido);
+                } else {
+                    params.put("param3[]", "apellido");
+                }
+                if (correo != null) {
+                    params.put("param4[]", correo);
+                } else {
+                    params.put("param4[]", "email" + userid + "@email.com");
+                }
+                params.put("param5[]", tlf);
+
+                return params;
+            }
+
+
+        };
+        sr.setRetryPolicy(new DefaultRetryPolicy(12000 * 27000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+        queue.add(sr);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_sincronizar:
+                int count = 0;
+                for (int i = 0; i < rows.size(); i++) {
+                    if (rows.get(i).isChecked()) {
+                        count++;
+                    }
+                }
+
+                if (count > 0) {
+                    showProgressDialog(getString(R.string.sincronizar_contactos));
+                    for (int i = 0; i < rows.size(); i++) {
+                        if (rows.get(i).isChecked()) {
+                            synContacto(getActivity(), rows.get(i).getUserid(), rows.get(i).getName(),
+                                    rows.get(i).getSurname(), rows.get(i).getEmail(), rows.get(i).getMobile_number());
+                            pendingRequests++;
+                        }
+                    }
+
+                } else {
+                    Toast.makeText(getActivity(), "Debe seleccionar al menos un(1) contacto", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.btn_selecctodos:
+                selectAll();
+                break;
+            case R.id.btn_deselecctodos:
+                unSelectAll();
+                break;
+        }
+    }
+}
