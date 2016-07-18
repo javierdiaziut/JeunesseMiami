fbd2e07373c1d13c08b2d88a2eee7f75373fbf43
Javier Diaz <ibmjavier89@gmail.com>
2016-07-18 08:39:40 -0430

    Tercera fase: Envio de MMS y se adjunta la imagen

--- app/src/main/java/com/appcontactos/javierdiaz/jeunessemiami/fragments/PlantillasFragment.java
+++ app/src/main/java/com/appcontactos/javierdiaz/jeunessemiami/fragments/PlantillasFragment.java
@@ -1,15 +1,29 @@
 package com.appcontactos.javierdiaz.jeunessemiami.fragments;
 
+import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,6 +31,12 @@ import android.widget.Toast;
 import com.appcontactos.javierdiaz.jeunessemiami.R;
 import com.appcontactos.javierdiaz.jeunessemiami.activities.NavigationActivity;
 import com.appcontactos.javierdiaz.jeunessemiami.adaptadores.CustomPlantillasAdapter;
+import com.appcontactos.javierdiaz.jeunessemiami.util.Util;
+import com.squareup.picasso.Picasso;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlantillasFragment extends Fragment {
     /**
@@ -25,8 +45,13 @@ public class PlantillasFragment extends Fragment {
     protected ProgressDialog mProgressDialog;
     private CustomPlantillasAdapter customPlantillasAdapter;
     private ListView listViewPlantillas;
-    private Button btnPreview;
-    private TextView txtPreviewmsg, txtPreviewlinkvideo, txtPreviewimg;
+    private Button btnPreview, btnSend;
+    private TextView txtPreviewmsg, txtPreviewlinkvideo;
+    private ImageView imgPreviewimg;
+    public static final int PERMISSIONS_REQUEST = 10;
+    private static final int REQUEST_A_PICTURE = 100;
+    private Uri actualURIimg;
+    private String actualPATHimg = "";
 
 
     public PlantillasFragment() {
@@ -41,12 +66,13 @@ public class PlantillasFragment extends Fragment {
         mProgressDialog = new ProgressDialog(getContext());
         listViewPlantillas = (ListView) view.findViewById(R.id.listview_plantillas);
         btnPreview = (Button) view.findViewById(R.id.btn_preview_msg);
+        btnSend = (Button) view.findViewById(R.id.btn_send_msg);
         txtPreviewmsg = (TextView) view.findViewById(R.id.txtview_preview_msg);
         txtPreviewlinkvideo = (TextView) view.findViewById(R.id.txtview_preview_vid);
-        txtPreviewimg = (TextView) view.findViewById(R.id.textView_preview_img);
+        imgPreviewimg = (ImageView) view.findViewById(R.id.imgView_preview_img);
 
 
-        if(NavigationActivity.plantillasMensajes != null && NavigationActivity.plantillasMensajes.size() > 0){
+        if (NavigationActivity.plantillasMensajes != null && NavigationActivity.plantillasMensajes.size() > 0) {
             listViewPlantillas.setItemsCanFocus(true);
             customPlantillasAdapter = new CustomPlantillasAdapter(getContext(), NavigationActivity.plantillasMensajes);
             listViewPlantillas.setAdapter(customPlantillasAdapter);
@@ -56,10 +82,9 @@ public class PlantillasFragment extends Fragment {
         btnPreview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                for(int i =0; i < NavigationActivity.plantillasMensajes.size(); i++){
-                    if(NavigationActivity.plantillasMensajes.get(i).isChecked()){
+                for (int i = 0; i < NavigationActivity.plantillasMensajes.size(); i++) {
+                    if (NavigationActivity.plantillasMensajes.get(i).isChecked()) {
                         txtPreviewmsg.setText(NavigationActivity.plantillasMensajes.get(i).getDescripcion());
-                        txtPreviewimg.setText(NavigationActivity.plantillasMensajes.get(i).getImagen());
                         txtPreviewlinkvideo.setText(NavigationActivity.plantillasMensajes.get(i).getLink_video());
 
                         break;
@@ -68,6 +93,56 @@ public class PlantillasFragment extends Fragment {
             }
         });
 
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mensaje = "";
+
+                for (int i = 0; i < NavigationActivity.plantillasMensajes.size(); i++) {
+                    if (NavigationActivity.plantillasMensajes.get(i).isChecked()) {
+                        mensaje = NavigationActivity.plantillasMensajes.get(i).getDescripcion() + " Video_link: " + NavigationActivity.plantillasMensajes.get(i).getLink_video();
+
+                        break;
+                    }
+                }
+                if(mensaje.isEmpty()){
+                    Toast.makeText(getContext(), "Debe seleccionar una plantilla de mensaje", Toast.LENGTH_LONG).show();
+                }else {
+                    sendMMS(mensaje);
+                }
+
+            }
+        });
+
+        imgPreviewimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    int permissionCamCheck = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA);
+                    int permissionGalCheck = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                    if (permissionCamCheck != PackageManager.PERMISSION_GRANTED || permissionGalCheck != PackageManager.PERMISSION_GRANTED) {
+                        List<String> permissionsNeeded = new ArrayList<String>();
+                        permissionsNeeded.add(Manifest.permission.CAMERA);
+                        permissionsNeeded.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+                        requestPermissions(permissionsNeeded.toArray(new String[permissionsNeeded.size()]),
+                                PERMISSIONS_REQUEST);
+                    } else {
+                        Intent i = new Intent(
+                                Intent.ACTION_PICK,
+                                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                        startActivityForResult(i, 200);
+                    }
+                } else {
+                    Intent i = new Intent(
+                            Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                    startActivityForResult(i, 200);
+                }
+            }
+        });
         return view;
 
 
@@ -98,5 +173,164 @@ public class PlantillasFragment extends Fragment {
         } else {
             Log.e("MainActivity.class", "Error al mostrar el Progress Dialog");
         }
+    }
+
+    private void sendMMS(String mensaje) {
+        String numeros = "";
+        for (int i = 0; i < NavigationActivity.rows.size(); i++) {
+            if (NavigationActivity.rows.get(i).isChecked()) {
+                numeros += NavigationActivity.rows.get(i).getMobile_number() + ";";
+            }
+        }
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.putExtra("address", numeros);
+        i.putExtra("sms_body", mensaje);
+        i.putExtra(Intent.EXTRA_STREAM, actualURIimg);
+        i.setType("image/png");
+        startActivity(i);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (null != data) {
+            Uri selectedImage = data.getData();
+            actualURIimg = selectedImage;
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getActivity().getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            actualPATHimg = picturePath;
+            cursor.close();
+
+            imgPreviewimg.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+        }
+    }
+
+    public static String getPath(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+
+                // TODO handle non-primary volumes
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{
+                        split[1]
+                };
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+
+            // Return the remote address
+            if (isGooglePhotosUri(uri))
+                return uri.getLastPathSegment();
+
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is Google Photos.
+     */
+    public static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+    }
+
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
     }
 }
